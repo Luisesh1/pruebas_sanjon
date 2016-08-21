8e886ca24d12154cfc53ecd4084577f22269be71
luisesh11 <luisesh1x10@hotmail.com>
2016-08-21 06:25:16 +0000

    no se que rollo

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,7 +13,8 @@ class CategoriesController < ApplicationController
       getPedidos(@category.id)
   end
   def getPedidos(id)
-     @pedidos= SaucerOrder.joins(:platillo=>:category).where("categories.id = ?",id).order(:created_at)
+     @pedidos= SaucerOrder.joins(:platillo=>:category).where("categories.id = ?",id).where.not("categories.id = ?",3).order(:created_at)
+     #@pedidos= SaucerOrder.joins(:platillo=>:category).where("categories.id = ? and status is not ? and status is not ?",id,3,4).order(:created_at)
     respond_to do |format|
           format.html {}
           format.json { render :json=> @pedidos  }
